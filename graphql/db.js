b193e53b5e3e7ae1432b650372cf267af37c47f0
jsjsjskjs <jsjsjskjs@gmail.com>
2022-02-04 04:41:45 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,94 +1,45 @@
-export const people = [
-  {
-    id: '0',
-    name: 'Nicolas',
-    age: 18,
-    gender: 'female'
-  },
-  {
-    id: '1',
-    name: 'Jisu',
-    age: 18,
-    gender: 'female'
-  },
-  {
-    id: '2',
-    name: 'Japan Guy',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '3',
-    name: 'Daal',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '4',
-    name: 'JD',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '5',
-    name: 'moondaddi',
-    age: 18,
-    gender: 'male'
-  },
-  {
-    id: '6',
-    name: 'flynn',
-    age: 18,
-    gender: 'male'
-  }
-]
+import axios from 'axios'
+const BASE_URL = 'https://yts-proxy.now.sh/'
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`
 
-let movies = [
-  {
-    id: 0,
-    name: 'Star Wars - The new one',
-    score: 1
-  },
-  {
-    id: 1,
-    name: 'Avengers - The new one',
-    score: 8
-  },
-  {
-    id: 2,
-    name: 'the godfather i',
-    score: 99
-  },
-  {
-    id: 3,
-    name: 'logan',
-    score: 2
-  }
-]
-
-export const getById = (id) => {
-  const filteredMovies = movies.filter((el) => id === el.id)
-  return filteredMovies[0]
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  })
+  return movies
 }
 
-export const getMovies = () => movies
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((el) => id !== el.id)
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies
-    return true
-  } else {
-    return false
-  }
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  })
+  return movie
 }
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  }
-  movies.push(newMovie)
-  return newMovie
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  })
+  return movies
 }
